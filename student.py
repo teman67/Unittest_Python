3ff58d0a3e90459403a5d6c42f8cd7e477a8a1bc
teman67 <>
2023-11-06 11:46:23 +0000

    add extention test

--- student.py
+++ student.py
@@ -21,3 +21,6 @@ class Student:
 
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extention(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
--- test_students.py
+++ test_students.py
@@ -1,21 +1,45 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 
 class TestStudent(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+
+    def setUp(self):
+        print("setup")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
+
+    def tearDown(self):
+        print("tear down")
+
     def test_full_name(self):
-        student = Student("Ali", "Mali")
-        self.assertEqual(student.full_name, "Ali Mali")
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student = Student("Ali", "Mali")
-        student.alert_santa()
-
-        self.assertTrue(student.naughty_list)
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
     def test_email(self):
-        student = Student("Ali", "Mali")
-        self.assertEqual(student.email, "ali.mali@email.com")
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
+
+    def test_apply_extention(self):
+        print("test_apply_extention")
+        old_end_date = self.student.end_date
+        self.student.apply_extention(5)
+
+        self.assertEqual(self.student.end_date,
+                         old_end_date + timedelta(days=(5)))
 
 
 if __name__ == "__main__":
