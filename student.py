ff5ae5bfd8a307aa606b7a615afea09c7d652819
teman67 <>
2023-11-06 12:03:49 +0000

    text mock

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
@@ -22,5 +23,14 @@ class Student:
     def alert_santa(self):
         self.naughty_list = True
 
-    def apply_extention(self, days):
-        self.end_date = self.end_date + timedelta(days=days)
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
